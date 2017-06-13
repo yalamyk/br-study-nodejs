ccf3920f8357ad4329c82d87c961fd22a14ff2b7
권보람 <yalamyk@gmail.com>
2017-06-13 16:03:15 +0900

    글편집2(기능)

--- expressTest/myapp/app_orientdb.js
+++ expressTest/myapp/app_orientdb.js
@@ -47,10 +47,6 @@ app.post('/upload', upload.single('userfile'), function(req,res){
 app.get('/topic/add',function(req,res){
 	var sql = "SELECT FROM topic";
 	db.query(sql).then(function(topics){
-		if(topics.length == 0){
-			console.log('There us bi topic record.');
-			res.status(500).send('Internal Server Error'); //send가 실행되면 다음 코드는 실행되지 않는다.
-		}
 		res.render('add',{topics:topics});
 	});
 	
@@ -69,6 +65,37 @@ app.post('/topic/add',function(req,res){
 	}).then(function(results){
 		res.redirect('/topic/'+encodeURIComponent(results[0]['@rid']));
 	});
+});
+app.get('/topic/:id/edit',function(req,res){
+	var sql = "SELECT FROM topic";
+	var id = req.params.id;
+	db.query(sql).then(function(topics){
+		var sql = "SELECT FROM topic WHERE @rid=:rid";
+		db.query(sql, {params:{rid:id}}).then(function(topic){
+			res.render('edit',{topics:topics, topic:topic[0]});
+		});
+		
+	});
+	
+});
+app.post('/topic/:id/edit',function(req,res){
+	var sql = "UPDATE topic SET title=:t, description=:d, author=:a WHERE @rid=:rid";
+	var id = req.params.id;
+	var title = req.body.title;
+	var desc = req.body.description;
+	var author = req.body.author;
+	db.query(sql,{
+		params:{
+			t:title,
+			d:desc,
+			a:author,
+			rid:id
+		}
+	}).then(function(topics){
+		res.redirect('/topic/'+encodeURIComponent(id));
+		
+	});
+	
 });
 //topic, topic/:id 두가지 경우의 url을 받았을경우.
 //아래의 문제가 있기 때문에 이렇게 작성한다.
