594f87352902e5293f14d5e20655f55affb116f1
권보람 <yalamyk@gmail.com>
2017-06-14 16:47:29 +0900

    node-mysql : select insert update delete 추가

--- expressTest/myapp/database_mysql.js
+++ expressTest/myapp/database_mysql.js
@@ -9,21 +9,54 @@ var conn = mysql.createConnection({
 });
 
 conn.connect(); // 에러 안나면 ok
-var sql = "SELECT * FROM topic";
-conn.query(sql, function(err, rows, fields){
+
+//*********************<mysql databse 확인 : select>
+// var sql = "SELECT * FROM topic";
+// conn.query(sql, function(err, rows, fields){
+// 	if(err){
+// 		console.log(err);
+// 	}else{
+// 		//console.log('rows',rows);
+// 		//rows : array임. [{행},{행}...]
+// 		//console.log('fields',fields);
+// 		// 해당 컬럼에 대한 상세 정보
+// 		for(var i=0; i<rows.length; i++){
+// 			console.log(rows[i].author);
+// 		}
+// 	}
+// });
+
+//*********************<mysql databse 추가 : insert>
+// var sql = "INSERT INTO topic (title, description, author) VALUES(?,?,?)";
+// var params = ['Supervisor', 'Watcher', 'graphittie'];
+// conn.query(sql, params, function(err, rows, fileds){
+// 	if(err){
+// 		console.log(err);
+// 	}else{
+// 		console.log(rows.insertId);
+// 		//rows : OkPacket{}
+// 	}
+// });
+
+//*********************<mysql databse 수정, 삭제 >
+// var sql = 'UPDATE topic SET title=?, author=? WHERE id=?';
+// var params = ['NPM','leezche',1];
+// conn.query(sql, params, function(err, rows, fields){
+// 	if(err){
+// 		console.log(err);
+// 	}else{
+// 		console.log(rows);
+// 		//rows : OkPacket{}
+// 	}
+// });
+var sql = 'DELETE FROM topic WHERE id=?';
+var params = [1];
+conn.query(sql, params, function(err, rows, fields){
 	if(err){
 		console.log(err);
 	}else{
-		console.log('rows',rows);
-		//rows : array임. [{행},{행}...]
-		console.log('fields',fields);
-		// 해당 컬럼에 대한 상세 정보
+		console.log(rows);
+		//rows : OkPacket{}
 	}
 });
 conn.end(); //접속 끊김.
-// conn.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
-
-// conn.end();
