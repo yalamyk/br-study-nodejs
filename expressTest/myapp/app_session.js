dea1a9924969a6c3bf27f1ab84d967749c412295
권보람 <yalamyk@gmail.com>
2017-06-19 16:32:47 +0900

    session2 - login까지 추가

--- expressTest/myapp/app_session.js
+++ expressTest/myapp/app_session.js
@@ -2,7 +2,9 @@ var express = require('express');
 var session = require('express-session');
 //npm install express-session --save : express는 session을 지원하지 않는다.
 
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(session({
   secret: '1234481723!@#!@#thisiskeyvalue',
   resave: false,
@@ -19,7 +21,58 @@ app.get('/count',function(req,res){
 });
 app.get('/tmp',function(req,res){
 	res.send('result:'+req.session.count); //읽어 올 수 있다.
-})
+});
+app.get('/auth/login',function(req,res){
+	var output = `
+		<h1>Login</h1>
+		<form action="/auth/login" method="post">
+			<p>
+				<input type="text" name="username" placeholder="username">
+			</p>
+			<p>
+				<input type="password" name="password" placeholder="password"/>
+			</p>
+			<p>
+				<input type="submit">
+			</p>
+		</form>
+	`;
+	res.send(output);
+});
+app.post('/auth/login',function(req,res){
+	var user= {
+		username:'boram',
+		password:'123',
+		displayName:'boram-display'
+	};
+	var uname = req.body.username;
+	var pwd = req.body.password;
+	//실제로는 database가 개입하게 된다.
+	if(uname === user.username && pwd === user.password){
+		//res.send('hello master');
+		req.session.displayName = user.displayName;
+		res.redirect('/welcome');
+	}else{
+		res.send('who are you? <a href="/auth/login">login</a>');
+	}
+	//res.send(uname);
+});
+app.get('/welcome',function(req,res){
+	if(req.session.displayName){
+		res.send(`
+			<h1> Hello, ${req.session.displayName} </h1>
+			<a href="/auth/logout">logout</a>
+		`);
+	}else{
+		res.send(`
+			<h1>Welcome</h1>
+			<a href="/auth/login">login</a>
+		`);
+	}
+	res.send(req.session);
+});
+app.get('/w')
+
 app.listen(3303, function(){
 	console.log('Connect 3303 port!!');
 });
